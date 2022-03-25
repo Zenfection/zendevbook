0a7d7b54a5c322bd19d4c5b7810181c6588e4b54
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2022-03-25 23:18:11 +0700

    update

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -8,13 +8,26 @@ const isProd = process.env.NODE_ENV === 'production'
 export default defineUserConfig<DefaultThemeOptions>({
   base: '/',
   head: [
-    ['link',{rel: 'icon',type: 'image/png',sizes: '16x16',href: `/images/icons/favicon-16x16.png`,},],
-    ['link', { rel: 'icon',type: 'image/png',sizes: '32x32',href: `/images/icons/favicon-32x32.png`,},],
+    [ 'link',
+      {
+        rel: 'icon', type: 'image/png', sizes: '16x16', href: `/images/icons/favicon-16x16.png`,
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'icon', type: 'image/png', sizes: '32x32', href: `/images/icons/favicon-32x32.png`,
+      },
+    ],
     ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
-    ['meta', { name: 'application-name', content: 'ZenDevBook' }],
-    ['meta', { name: 'apple-mobile-web-app-title', content: 'ZenDevBook' }],
-    ['meta',{ name: 'apple-mobile-web-app-status-bar-style', content: 'black' },],
-    ['link',{ rel: 'apple-touch-icon', href: `/images/icons/apple-touch-icon.png` },],
+    ['meta', { name: 'application-name', content: 'ZenGuide' }],
+    ['meta', { name: 'apple-mobile-web-app-title', content: 'ZenGuide' }],
+    ['meta',
+      { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
+    ],
+    ['link',
+      { rel: 'apple-touch-icon', href: `/images/icons/apple-touch-icon.png` },
+    ],
     ['link', { rel: 'mask-icon', href: '/images/icons/safari-pinned-tab.svg' }],
     ['meta', { name: 'msapplication-TileColor', content: '#06bdf8' }],
     ['meta', { name: 'theme-color', content: '#06bdf8' }],
@@ -24,28 +37,31 @@ export default defineUserConfig<DefaultThemeOptions>({
   locales: {
     '/': {
       lang: 'vi',
-      title: 'Zen DevBook Guide',
+      title: 'Zen Dev Guide',
       description:
-        'Cung cấp hướng dẫn cho từng công nghệ hay ngôn ngữ lập trình',
-    }
+        'Nơi Hướng dẫn học lập trình từng môn như: HTML, CSS, Javascript, NodeJS, ReactJS ...',
+    },
   },
 
   bundler:
+    // specify bundler via environment variable
     process.env.DOCS_BUNDLER ??
+    // use vite in dev, use webpack in prod
     '@vuepress/vite',
 
   themeConfig: {
     logo: '/images/hero.png',
+    lang: 'vi',
     docsRepo: 'zenfection/zendevbook',
     editLink: false,
     docsDir: 'docs',
-    theme: 'dark',
     // theme-level locales config
     locales: {
-      '/vi/': {
+      '/': {
         navbar: navbar.vi,
         sidebar: sidebar.vi,
 
+        // page meta
         lastUpdatedText: 'Cập nhật lúc ',
         contributorsText: 'Tác giả',
       },
@@ -64,17 +80,18 @@ export default defineUserConfig<DefaultThemeOptions>({
   },
 
   plugins: [
-    // ['@vuepress/plugin-debug'],
-    ['@vuepress/plugin-docsearch', {
+    [
+      '@vuepress/plugin-docsearch',
+      {
         appId: '3CJDV2AFXL',
         apiKey: '4f7f93d347463109c3b6fd21d3ac2424',
-        indexName: 'zendevguide',
-        // placeholder: 'Tìm Kiếm...',
+        indexName: 'ctuvn',
+        placeholder: 'Tìm Kiếm...',
       },
     ],
-    ['@vuepress/plugin-google-analytics', { id: 'G-KXJDM0Z98W' }],
+    ['@vuepress/plugin-google-analytics', { id: 'G-1GE3D8ZGM4' }],
     ['@vuepress/plugin-pwa'],
-    [ '@vuepress/plugin-register-components', { componentsDir: path.resolve(__dirname, './components') }, ],
+    ['@vuepress/plugin-register-components', { componentsDir: path.resolve(__dirname, './components') },],
     ['vuepress-plugin-zenlive2d-cat'],
     ['@snippetors/vuepress-plugin-tabs'],
   ],
--- docs/.vuepress/configs/navbar/vi.ts
+++ docs/.vuepress/configs/navbar/vi.ts
@@ -1,9 +1,36 @@
 import type { NavbarConfig } from '@vuepress/theme-default'
 
-
 export const vi: NavbarConfig = [
   {
-    text: 'Trang Chủ',
-    link: '/',
+    text: 'WebSite',
+    children: [
+      {
+        text: 'Cơ bản',
+        children: [
+          { text: 'HTML', link: '/web/htlm/' },
+          { text: 'CSS', link: '/web/css/' },
+          { text: 'Javascript', link: '/web/javascript/' },
+        ]
+      },
+      {
+        text: 'Thư viện',
+        children: [
+          { text: 'Bootstrap 5', link: '/web/bootstrap5/' },
+          { text: 'jQuery', link: '/web/jquery/' },
+        ]
+      }
+    ]
   },
+
+  {
+    text: 'FrontEnd',
+    children: [
+      { text: 'Typescript', link: '/frontend/typescript/' },
+      { text: 'NodeJS', link: '/frontend/nodejs/' },
+      { text: 'VueJS', link: '/frotned/vue/' },
+    ]
+  },
+
+  {text: 'Linux', link: '/linux/'},
+  {text: 'Java', link: '/java/'},
 ]
